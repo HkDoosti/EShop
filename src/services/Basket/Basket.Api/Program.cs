2fe33dfbf6b280f4a214e0e61bb9fd94727fdadf
HkDoosti <hakimedusti@gmail.com>
2024-11-01 09:08:08 +0330

    #feat Add Basket service to project

--- src/services/Basket/Basket.Api/Program.cs
+++ src/services/Basket/Basket.Api/Program.cs
@@ -1,34 +1,25 @@
 
-namespace Basket.Api
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisCache(act =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    act.Configuration = builder.Configuration.GetValue<string>("CashSettings:ConnectionString");
+});
+var app = builder.Build();
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
+
--- src/services/Inventory/Inventory.Api/GlobalUsing.cs
+++ src/services/Inventory/Inventory.Api/GlobalUsing.cs
@@ -1,26 +1,25 @@
 ﻿//----------System
-global using System.Reflection;
-//----------Microsoft
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.AspNetCore.Mvc;
-//----------Inventory
-global using Inventory.Api;
-global using Inventory.Domain.Shared;
-global using Inventory.Application;
+//-----------FluentValidation
+global using FluentValidation;
 global using Inventory.Api.Extensions;
-global using Inventory.Infrastructure.Data;
-global using Inventory.Application.IRepository;
-global using Inventory.Infrastructure.Repository;
-global using Inventory.Application.IRepository.ICustomRepositories;
-global using Inventory.Infrastructure.Repository.CustomRepositories;
+global using Inventory.Application;
 global using Inventory.Application.Behaviors;
 global using Inventory.Application.Command.CategoryCommand.AddCategoryCommand;
 global using Inventory.Application.Command.CategoryCommand.DeleteCategoryCommand;
 global using Inventory.Application.Command.CategoryCommand.EditCategoryCommand;
+global using Inventory.Application.IRepository;
+global using Inventory.Application.IRepository.ICustomRepositories;
+//----------Inventory
+global using Inventory.Domain.Shared;
+global using Inventory.Infrastructure.Data;
+global using Inventory.Infrastructure.Repository;
+global using Inventory.Infrastructure.Repository.CustomRepositories;
+//-----------MediatR
+global using MediatR;
+global using Microsoft.AspNetCore.Mvc;
+//----------Microsoft
+global using Microsoft.EntityFrameworkCore;
 //-----------SharedProject
 global using SharedProject.ActionFilters;
 global using SharedProject.Middlewars;
-//-----------MediatR
-global using MediatR;
-//-----------FluentValidation
-global using FluentValidation;
+global using System.Reflection;
